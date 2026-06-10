065890927274778d767e568aa7ff7afd7038ef3e
balavick <balavigneshg28@gmail.com>
2026-06-10 14:16:28 +0530

    Project -1

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("BV Electronics Platform - Initialized");
+
+const exploreButton = document.getElementById("explore-btn");
+
+exploreButton.addEventListener("click", () => {
+    alert("Opening the BV Electronics Product Catalog!");
+});
